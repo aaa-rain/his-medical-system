3e349b76877c34373da321fef50070bff17767cc
4003240107-顾运雨 <3066962936@qq.com>
2026-06-19 11:48:03 +0800

    初始提交：搭建SpringBoot项目框架

---
+++ src/main/java/org/example/controller/TestController.java
@@ -0,0 +1,22 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public Map<String, Object> hello() {
+        Map<String, Object> result = new HashMap<>();
+        result.put("code", 200);
+        result.put("message", "HIS医疗系统运行成功！");
+        result.put("data", "Hello, 这是你的第一个接口！");
+        return result;
+    }
+}
